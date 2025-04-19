f9c88ca6044a9e6781e107e189d6065bfbb27c9d
Carlos Ferreira <leopardotm@outlook.com>
2025-04-19 20:16:42 -0300

    feat(core): implement main App component with routing setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Index from './Index';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Social from './pages/Social';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/social" element={<Social />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App; 
